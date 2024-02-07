91ec38766ad2a5a60ae8d9ee03297fd6d9fda031
Amrish-G <amrishguru2512@gmail.com>
2024-02-07 14:39:44 +0530

    My first Commit

---
+++ 2_Feb_2024/Task01.py
@@ -0,0 +1,30 @@
+# Task-2
+# Printing Multiplication table of "2" normally
+
+print("Printing multiplication table 2")
+print("2*1 = 2")
+print("2*2 = 4")
+print("2*3 = 6")
+print("2*4 = 8")
+print("2*5 = 10")
+print("2*6 = 12")
+print("2*7 = 14")
+print("2*8 = 16")
+print("2*9 = 18")
+print("2*10 = 20")
+print()
+
+# Printing multiplication table for number given by user and using printf function
+num = int(input("Enter number for running mulitplication table : "))
+print(f"{num} x 1 = {num*1}")
+print("{} x 2 = {}".format(num, num*2))
+print("{} x 3 = {}".format(num, num*3))
+print("{} x 4 = {}".format(num, num*4))
+print("{} x 5 = {}".format(num, num*5))
+print("{} x 6 = {}".format(num, num*6))
+print(f"{num} x 1 = {num*7}")
+print(f"{num} x 1 = {num*8}")
+print(f"{num} x 1 = {num*9}")
+print(f"{num} x 1 = {num*10}")
+
+
---
+++ 2_Feb_2024/Task02.py
@@ -0,0 +1,16 @@
+#Task-1
+#Getting name, roll_no, contact number, age as input from user
+
+print("Enter required details ")
+name = input("Please, Enter your name : ")
+age = input("Please, Enter your Age : ")
+roll_no = input("Enter your roll number : ")
+contact_no = input("Enter your contact number : ")
+print()
+print("Kindly validate entered details")
+print("Name : ",name)
+print("Age : ",age)
+print("Roll no : ",roll_no)
+print("Contact No : ",contact_no)
+
+
